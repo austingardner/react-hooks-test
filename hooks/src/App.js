66ea1ed825dcd09b76636dc96ecd643d64ca38cb
austingardner <austingardner66@gmail.com>
2018-12-20 17:16:18 -0800

    so many beans... as many as you want

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Button, Grid } from '@material-ui/core';
+import Bean from './pinto_bean.jpg';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const useStyles = makeStyles({
+  button: {
+    width: 200,
+    height: 50,
+    color: 'black',
+    background: 'white'
+  },
+  bean: {
+    width: 100,
+    height: 'auto'
+  },
+  root: {
+    width: '100%'
   }
-}
+});
 
-export default App;
+const makeBeans = (num, classes) => {
+  let beans = [];
+  for (let i = 0; i < num; i++) {
+    beans.push(<img src={Bean} alt="Bean" key={i} className={classes.bean} />);
+  }
+  return beans;
+};
+
+export const App = props => {
+  const classes = useStyles();
+  const [number, setNumber] = useState(10);
+
+  return (
+    <div className={classes.root}>
+      cool number of beans: {number} <br />
+      <Button className={classes.button} onClick={() => setNumber(number + 1)}>
+        Add A Bean
+      </Button>
+      <br />
+      <Button className={classes.button} onClick={() => setNumber(number - 1)}>
+        Take A Bean Away :(
+      </Button>
+      <br />
+      <Grid>
+        {makeBeans(number, classes).map((obj, i) => {
+          return obj;
+        })}
+      </Grid>
+    </div>
+  );
+};
--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
+import { install } from '@material-ui/styles';
+
+install();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
